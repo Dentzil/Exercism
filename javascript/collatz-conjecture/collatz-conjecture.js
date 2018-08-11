77ace5347a8a762906a7d743c0bdd36b00e9bac7
Denis Gerbel <denis.gerbel@gmail.com>
2018-08-11 14:45:04 +0200

    add javascript collatz-conjecture

---
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,27 @@
+class CollatzConjecture {
+    constructor() { }
+
+    steps(number) {
+        if (number <= 0) {
+            throw new Error("Only positive numbers are allowed");
+        }
+
+        let steps = 0;
+
+        while (number != 1)
+        {
+            if (number % 2 == 0) {
+                number = Math.floor(number / 2);
+            }
+            else {
+                number = 3 * number + 1;
+            }
+
+            steps++;
+        }
+
+        return steps;
+    }
+};
+
+module.exports = CollatzConjecture;
