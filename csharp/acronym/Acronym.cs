444850c0aff292bb67ae7dc944d20edec3fbc54f
Denis Gerbel <denis.gerbel@gmail.com>
2018-08-12 20:42:32 +0200

    add csharp acronym

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var singleWords = Regex.Split(phrase, "[^a-zA-Z]")
+                               .Where(e => e != string.Empty)
+                               .ToList();
+
+        return string.Join("", singleWords.Select(e =>  char.ToUpper(e[0])));
+    }
+}
---
+++ csharp/acronym/AcronymTest.cs
@@ -0,0 +1,34 @@
+using Xunit;
+
+public class AcronymTest
+{    
+    [Fact]
+    public void Basic()
+    {
+        Assert.Equal("PNG", Acronym.Abbreviate("Portable   Network Graphics"));
+    }
+
+    [Fact]
+    public void Lowercase_words()
+    {
+        Assert.Equal("ROR", Acronym.Abbreviate("Ruby on Rails"));
+    }
+
+    [Fact]
+    public void Punctuation()
+    {
+        Assert.Equal("FIFO", Acronym.Abbreviate("First In, First Out"));
+    }
+
+    [Fact]
+    public void All_caps_word()
+    {
+        Assert.Equal("GIMP", Acronym.Abbreviate("GNU Image Manipulation Program"));
+    }
+
+    [Fact]
+    public void Punctuation_without_whitespace()
+    {
+        Assert.Equal("CMOS", Acronym.Abbreviate("Complementary metal-oxide semiconductor"));
+    }
+}
