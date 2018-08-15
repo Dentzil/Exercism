e44fe661562a220db2c9792d9fef7cb63fba4659
Denis Gerbel <denis.gerbel@gmail.com>
2018-08-15 20:13:02 +0200

    csharp robot-name refactoring

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,50 +1,39 @@
-﻿namespace Exercism_robot_name
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Robot
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+    private const int LettersLength = 2;
+    private const int NumbersLength = 3;
 
-    public class Robot
+    private static Random rand = new Random();
+    private static HashSet<string> robotNames = new HashSet<string>();
+
+    public string Name { get; private set; }
+
+    public Robot()
     {
-        private static Random rand = new Random();
+        GenerateRobotName();
+    }
 
-        private static HashSet<string> robotNames = new HashSet<string>();
+    public void Reset()
+    {
+        robotNames.Remove(Name);
+        Name = null;
+    }
 
-        private string _name;
+    private void GenerateRobotName()
+    {
+        string generatedName;
 
-        public string Name
+        do
         {
-            get
-            {
-                if (_name == null)
-                {
-                    GenerateRobotName();
-                }
+            generatedName = string.Empty;
+            generatedName += string.Concat(Enumerable.Range(0, LettersLength).Select(e => (char)rand.Next(65, 91)));
+            generatedName += string.Concat(Enumerable.Range(0, NumbersLength).Select(e => rand.Next(0, 10)));
+        } while (robotNames.Add(generatedName));
 
-                return _name;
-            }
-        }
-
-        public void Reset()
-        {
-            robotNames.Remove(_name);
-
-            _name = null;
-        }
-
-        private void GenerateRobotName()
-        {
-            string generatedName = null;
-
-            do
-            {
-                generatedName += string.Concat(Enumerable.Range(0, 2).Select(e => (char)rand.Next(65, 91)));
-                generatedName += string.Concat(Enumerable.Range(0, 3).Select(e => rand.Next(0, 10)));
-            } while (robotNames.Contains(generatedName));
-
-            robotNames.Add(generatedName);
-
-            _name = generatedName;
-        }
+        Name = generatedName;
     }
 }
