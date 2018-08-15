d1108661781574c27d2d6bf07cbe49ca0d848a2d
Denis Gerbel <denis.gerbel@gmail.com>
2018-08-15 20:16:50 +0200

    csharp collatz-conjecture using xUnit

--- csharp/collatz-conjecture/CollatzConjectureTest.cs
+++ csharp/collatz-conjecture/CollatzConjectureTest.cs
@@ -1,46 +1,41 @@
-﻿namespace Tests
+using System;
+using Xunit;
+
+public class CollatzConjectureTest
 {
-    using NUnit.Framework;
-    using System;
-    using Exercism_collatz_conjecture;
-
-    [TestFixture]
-    public class CollatzConjectureTest
+    [Fact]
+    public void Zero_steps_for_one()
     {
-        [Test]
-        public void Zero_steps_for_one()
-        {
-            Assert.AreEqual(0, CollatzConjecture.Steps(1));
-        }
+        Assert.Equal(0, CollatzConjecture.Steps(1));
+    }
 
-        [Test]
-        public void Divide_if_even()
-        {
-            Assert.AreEqual(4, CollatzConjecture.Steps(16));
-        }
+    [Fact]
+    public void Divide_if_even()
+    {
+        Assert.Equal(4, CollatzConjecture.Steps(16));
+    }
 
-        [Test]
-        public void Even_and_odd_steps()
-        {
-            Assert.AreEqual(9, CollatzConjecture.Steps(12));
-        }
+    [Fact]
+    public void Even_and_odd_steps()
+    {
+        Assert.Equal(9, CollatzConjecture.Steps(12));
+    }
 
-        [Test]
-        public void Large_number_of_even_and_odd_steps()
-        {
-            Assert.AreEqual(152, CollatzConjecture.Steps(1000000));
-        }
+    [Fact]
+    public void Large_number_of_even_and_odd_steps()
+    {
+        Assert.Equal(152, CollatzConjecture.Steps(1000000));
+    }
 
-        [Test]
-        public void Zero_is_an_error()
-        {
-            Assert.Throws<ArgumentException>(() => CollatzConjecture.Steps(0));
-        }
+    [Fact]
+    public void Zero_is_an_error()
+    {
+        Assert.Throws<ArgumentException>(() => CollatzConjecture.Steps(0));
+    }
 
-        [Test]
-        public void Negative_value_is_an_error()
-        {
-            Assert.Throws<ArgumentException>(() => CollatzConjecture.Steps(-15));
-        }
+    [Fact]
+    public void Negative_value_is_an_error()
+    {
+        Assert.Throws<ArgumentException>(() => CollatzConjecture.Steps(-15));
     }
 }
