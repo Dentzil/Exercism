55ad2ee68a51bb124a506d6aaedcacbbb8682a6e
Denis Gerbel <denis.gerbel@gmail.com>
2018-08-15 18:54:31 +0200

    csharp leap using xUnit

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 
-namespace Exercism_leap
+public static class Year
 {
-    public static class Year
+    public static bool IsLeap(int year)
     {
-        public static bool IsLeap(int year)
+        if (year < 1 || year > 9999)
         {
-            if (year < 1 || year > 9999)
-            {
-                throw new ArgumentOutOfRangeException(nameof(year));
-            }
-
-            return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
-
-            // Or just use System.DateTime.IsLeapYear(...)
+            throw new ArgumentOutOfRangeException(nameof(year));
         }
+
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+
+        // Or just use System.DateTime.IsLeapYear(...)
     }
 }
