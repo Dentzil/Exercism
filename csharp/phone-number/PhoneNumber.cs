da9877c3de5e8f9184ce116b6ac39a6197fa4ef8
Denis Gerbel <denis.gerbel@gmail.com>
2018-08-16 19:50:55 +0200

    csharp phone-number using xUnit

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -1,21 +1,18 @@
-﻿namespace Exercism_phone_number
+using System;
+using System.Text.RegularExpressions;
+
+public class PhoneNumber
 {
-    using System;
-    using System.Text.RegularExpressions;
-
-    public class PhoneNumber
+    public static string Clean(string number)
     {
-        public static string Clean(string number)
+        number = Regex.Replace(number, @"[^\d]", "");
+
+        var match = Regex.Match(number, @"^1?(?<ac>[2-9]\d{2})(?<ec>[2-9]\d{2})(?<sn>\d{4})$");
+        if (!match.Success)
         {
-            number = Regex.Replace(number, @"[^\d]", "");
-
-            var match = Regex.Match(number, @"^1?(?<ac>[2-9]\d{2})(?<ec>[2-9]\d{2})(?<sn>\d{4})$");
-            if (!match.Success)
-            {
-                throw new ArgumentException();
-            }
-
-            return $"{match.Groups["ac"].Value}{match.Groups["ec"].Value}{match.Groups["sn"].Value}";
+            throw new ArgumentException();
         }
+
+        return $"{match.Groups["ac"].Value}{match.Groups["ec"].Value}{match.Groups["sn"].Value}";
     }
 }
