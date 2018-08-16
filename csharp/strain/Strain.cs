d14f3f068e62f289f67f3080eb1271ebab87ba4a
Denis Gerbel <denis.gerbel@gmail.com>
2018-08-16 19:56:48 +0200

    csharp strain using xUnit

--- csharp/strain/Strain.cs
+++ csharp/strain/Strain.cs
@@ -1,29 +1,26 @@
-﻿namespace Exercism_strain
-{
-    using System;
-    using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
-    public static class Extenstions
+public static class Extenstions
+{
+    public static IEnumerable<T> Keep<T>(this IEnumerable<T> source, Func<T, bool> pred)
     {
-        public static IEnumerable<T> Keep<T>(this IEnumerable<T> source, Func<T, bool> pred)
+        foreach (var element in source)
         {
-            foreach (var element in source)
+            if (pred(element))
             {
-                if (pred(element))
-                {
-                    yield return element;
-                }
+                yield return element;
             }
         }
+    }
 
-        public static IEnumerable<T> Discard<T>(this IEnumerable<T> source, Func<T, bool> pred)
+    public static IEnumerable<T> Discard<T>(this IEnumerable<T> source, Func<T, bool> pred)
+    {
+        foreach (var element in source)
         {
-            foreach (var element in source)
+            if (!pred(element))
             {
-                if (!pred(element))
-                {
-                    yield return element;
-                }
+                yield return element;
             }
         }
     }
