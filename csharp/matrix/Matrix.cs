0d0b7f1a791cc722295c3722ff4e23d6b9a6de7e
Denis Gerbel <denis.gerbel@gmail.com>
2018-08-28 19:09:10 +0200

    add csharp matrix

---
+++ csharp/matrix/Matrix.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Matrix
+{
+    private readonly int[][] _matrix;
+
+    public Matrix(string input)
+    {
+        _matrix = ParseMatrix(input);
+        VerifyMatrix();
+
+        Rows = GetRowsCount();
+        Cols = GetColimnsCount();
+    }
+
+    public int Rows { get; }
+
+    public int Cols { get; }
+
+    public int[] Row(int row)
+    {
+        if (row < 0 || row >= Rows)
+        {
+            throw new ArgumentException($"Invalid row: {row}");
+        }
+
+        return _matrix[row];
+    }
+
+    public int[] Column(int col)
+    {
+        if (col < 0 || col >= Cols)
+        {
+            throw new ArgumentException($"Invalid column: {col}");
+        }
+
+        return _matrix.Select(e => e[col]).ToArray();
+    }
+
+    private int[][] ParseMatrix(string input)
+    {
+        var matrix = new List<int[]>();
+
+        var rows = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var row in rows)
+        {
+            var cols = row.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                          .Select(ParseElement)
+                          .ToArray();
+            
+            if (cols.Length > 0)
+            {
+                matrix.Add(cols);
+            }
+        }
+
+        return matrix.ToArray();
+    }
+
+    private int ParseElement(string element)
+    {
+        int intValue;
+        if (!int.TryParse(element, out intValue))
+        {
+            throw new ArgumentException($"Invalid matrix element: {element}");
+        }
+
+        return intValue;
+    }
+
+    private void VerifyMatrix()
+    {
+        if (IsEmpty())
+        {
+            throw new ArgumentException("Matrix is empty");
+        }
+
+        if (HasDifferentColumnsLength())
+        {
+            throw new ArgumentException("Matrix is invalid");
+        }
+    }
+
+    private bool IsEmpty()
+    {
+        return _matrix.Length == 0;
+    }
+
+    private bool HasDifferentColumnsLength()
+    {
+        return _matrix.Any(e => e.Length != _matrix[0].Length);
+    }
+
+    private int GetRowsCount()
+    {
+        return _matrix.Length;
+    }
+
+    private int GetColimnsCount()
+    {
+        return _matrix[0].Length;
+    }
+}
