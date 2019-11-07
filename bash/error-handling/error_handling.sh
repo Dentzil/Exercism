e0c4a351917487682e77eacd792c895d9eb659db
Denis Gerbel <denis.gerbel@gmail.com>
2019-11-07 20:06:33 +0100

    [bash] Added error-handling

---
+++ bash/error-handling/error_handling.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+error_handling() {
+    if [ "$#" -ne 1 ]; then
+        echo "Usage: ./error_handling <greetee>"
+        return 1
+    fi
+
+    echo "Hello, ${1}"
+}
+
+error_handling "$@"
