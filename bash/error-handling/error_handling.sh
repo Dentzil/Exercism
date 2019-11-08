62d7961b0b05e9ac29c6c7cfde33cf2e57927fe0
Denis Gerbel <denis.gerbel@gmail.com>
2019-11-08 21:28:05 +0100

    [bash] Updated error-handling

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-error_handling() {
-    if [ "$#" -ne 1 ]; then
+error_handling()
+{
+    if [[ "$#" != 1 ]]; then
         echo "Usage: ./error_handling <greetee>"
         return 1
     fi
