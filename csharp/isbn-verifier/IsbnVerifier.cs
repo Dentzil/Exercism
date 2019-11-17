5465c77e1bbba1eead512ffd1fc2e08ea79ea3b3
Denis Gerbel <denis.gerbel@gmail.com>
2019-11-17 20:43:46 +0100

    [csharp] Added isbn-verifier

---
+++ csharp/isbn-verifier/IsbnVerifier.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+public static class IsbnVerifier
+{
+    private static Regex _isbn10FormatRegex = new Regex(@"^\d{9}(\d|X)$");
+
+    public static bool IsValid(string number)
+    {
+        number = number.Replace("-", "");
+
+        if (_isbn10FormatRegex.IsMatch(number) == false)
+        {
+            return false;
+        }
+
+        int checkSum = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            checkSum += (number[i] == 'X' ? 10 : number[i] - 48) * (10 - i);
+        }
+
+        checkSum %= 11;
+
+        return checkSum == 0;
+    }
+}
